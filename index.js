4540e1ea1acffaf192e7cd85b7ce184f92079325
Arup Debnath <1805043@ugrad.cse.buet.ac.bd>
2023-05-21 21:15:23 +0600

    Added an api to get toy details by Id

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ async function run() {
             res.send(toys);
         });
 
+        app.get('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const toy = await toyCollection.findOne(query);
+            res.send(toy);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
