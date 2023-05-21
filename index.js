13a8f48e78c6421c6f07e22d1a6bd68f11d75906
Arup Debnath <1805043@ugrad.cse.buet.ac.bd>
2023-05-21 16:47:35 +0600

    Added an api to load all toys

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const toyCollection = client.db("animalArkade").collection("toys");
+
+        app.get('/toys', async (req, res) => {
+            const cursor = toyCollection.find({});
+            const toys = await cursor.toArray();
+            res.send(toys);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
